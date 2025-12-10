05af0fc3aa84421d53510b3a4987119d542bf043
Ева <davydova.04.eva@mail.ru>
2025-12-10 16:45:17 +0500

    Add example of environment.ts file

---
+++ my-app/src/environments/environment.example.ts
@@ -0,0 +1,16 @@
+export const environment = {
+// environment.ts
+  production: true,
+// environment.development.ts
+//   production: false,
+//   "serviceWorker": true,
+//   "ngswConfigPath": "ngsw-config.json",
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  }
+};
